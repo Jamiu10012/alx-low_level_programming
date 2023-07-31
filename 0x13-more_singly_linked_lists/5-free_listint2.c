8af93bdfa037f2c0aaf075168aecbd821a724a90
Jamiu Ibrahim <ibrahimjamiu026@gmail.com>
2023-07-31 12:05:18 -0700

    hrlp ooo

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * free_listint2 - frees a linked list
+ * free_listint2 - frees a linked list ha
  * @head: pointer to the listint_t list to be freed
  */
 void free_listint2(listint_t **head)
