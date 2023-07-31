4e0509508bcea381823f68356f73e503fe08fd86
Jamiu Ibrahim <ibrahimjamiu026@gmail.com>
2023-07-31 11:39:56 -0700

    done

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,17 +3,18 @@
 /**
  * print_listint - Display all the ellements in a linklist
  * @h: Type of listint to print
+ *
  * Return: number of nodes in list
  */
 
 size_t print_listint(const listint_t *h)
 {
-	size_t num_nodes = 0; /*initialize the instance*/
+	size_t num_nodes = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
-		num_nodes++; /*increment the counter*/
+		num_nodes++;
 		h = h->next;
 	}
 
