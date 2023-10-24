3690b9a140f9670a7c4572e6ad8293071e4d9ef3
Ibrahim Jamiu <ibrahimjamiu026@gmail.com>
2023-10-23 19:03:21 -0700

    stil

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -4,14 +4,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Define a structure for singly linked list nodes
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * bt jamiu
+ */
 typedef struct listint_s
 {
-int n; // Integer value held by the node
-struct listint_s *next; // Pointer to the next node in the list
+	int n;
+	struct listint_s *next;
 } listint_t;
 
-// Function prototypes for singly linked list operations
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
